4aa6b47efd54745f170f2f5780ce17eb44828307
Iliass Raihani <i.raihani@outlook.com>
2021-02-16 01:51:15 +0100

    people mvc

---
+++ Link.Asp.Mvc/Models/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Link.Asp.Mvc.Models
+{
+    public class People
+    {
+        public int Id { get; set; }
+
+    }
+}
