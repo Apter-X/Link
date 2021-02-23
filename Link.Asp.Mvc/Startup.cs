ee000dcc66dc2178bccdee9dd1b4f81813b1b093
Iliass Raihani <i.raihani@outlook.com>
2021-02-24 00:54:00 +0100

    remove policy

--- Link.Asp.Mvc/Startup.cs
+++ Link.Asp.Mvc/Startup.cs
@@ -35,11 +35,6 @@ namespace Link
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("RequireAdministratorRole",
-                     policy => policy.RequireRole("Administrator"));
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
