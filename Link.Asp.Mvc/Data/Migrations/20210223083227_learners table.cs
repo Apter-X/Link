9ac7efdcafc184d9ef84cee2e95457f4c40aad7a
Iliass Raihani <i.raihani@outlook.com>
2021-02-23 10:01:42 +0100

    add login component

---
+++ Link.Asp.Mvc/Data/Migrations/20210223083227_learners table.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Link.Data.Migrations
+{
+    public partial class learnerstable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "People",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "People",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "People",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "People",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "People");
+        }
+    }
+}
