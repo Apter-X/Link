d694acdb9312326782ec4f5c47f04181eaa6b10b
Iliass Raihani <i.raihani@outlook.com>
2022-04-09 18:12:20 +0000

    lebovsci switch project

---
+++ Link.MVC/Controllers/CalendarController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Link.MVC.Models;
+
+namespace Link.MVC.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private readonly ILogger<CalendarController> _logger;
+
+        public CalendarController(ILogger<CalendarController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
